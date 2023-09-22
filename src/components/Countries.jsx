8c17844b41b5f9f46b0d1e4dddc30cfa5faca136
mamatsa <otar.mamatsashvili@gmail.com>
2023-09-22 21:34:37 +0400

    feat: create drop-down list of countries

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,18 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Countries() {
+  const [countries, setCountries] = useState();
+  const [currentCountry, setCurrentCountry] = useState();
+
   useEffect(() => {
     const fetchCountries = async () => {
       const response = await fetch(
         "https://restcountries.com/v3.1/all?fields=name,"
       );
       const countries = await response.json();
-      return countries;
+      setCountries(countries);
     };
 
-    console.log(fetchCountries());
+    fetchCountries();
   }, []);
-  return <div>Hello from a child component!</div>;
+
+  const handleChange = (event) => {
+    setCurrentCountry(event.target.value);
+  };
+
+  return (
+    <form action="">
+      <select name="countries" id="countries" onChange={handleChange}>
+        {countries &&
+          countries.map((country, index) => {
+            const countryName = country.name.common;
+            return (
+              <option value={countryName} key={index}>
+                {countryName}
+              </option>
+            );
+          })}
+      </select>
+      <h1>{currentCountry}</h1>
+    </form>
+  );
 }
 
 export default Countries;
