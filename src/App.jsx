ad0850767e37dd40b43b3770bfe2ad5bbbe46518
mamatsa <otar.mamatsashvili@gmail.com>
2023-09-24 19:48:28 +0400

    feat: get user location latitude and longitude

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import Countries from "./components/Countries";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      });
+    } else {
+      console.log("Geolocation not supported");
+    }
+  }, []);
+
   return (
     <div className="flex justify-center p-6">
       <Countries />
